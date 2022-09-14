5989f632e68851064ad672cc88fcdf7f3e40037c
garaos <g.araos@gmail.com>
2022-09-13 22:02:27 -0300

    Correccion error al editar, renombrado de funciones, cambio color boton editar

--- main.js
+++ main.js
@@ -8,17 +8,17 @@ let alumnos = [];
 let alumnosOld = cargarPag();
 
 const btn_reg = document.querySelector("#btnAgregar");
-btn_reg.addEventListener("click", event => crear(event));
+btn_reg.addEventListener("click", event => create(event));
 
 const btn_ed = document.querySelector("#btnEditar");
-btn_ed.addEventListener("click", event => crear(event));
+btn_ed.addEventListener("click", event => create(event));
 
 
-function crear(event) {
+function create(event) {
     event.preventDefault();
     btn_reg.blur();
-    let datosRegistro = leerDatos();
-    let a = Object.values(datosRegistro);
+    let dataReg = readData();
+    let a = Object.values(dataReg);
     let b = a.some(e => e == '');
 
     if (this.alumnos) {
@@ -33,58 +33,59 @@ function crear(event) {
         alert("Favor complete toda la información");
     } else {
         if (filas === null) {
-            alumnos.push(datosRegistro);
+            alumnos.push(dataReg);
             localStorage.setItem("alumno", JSON.stringify(alumnos));
-            escDatos(datosRegistro);
+            writeData(dataReg);
         } else {
-            actualizando(datosRegistro);
+            actualize(dataReg);
+            filas = null;
         }
-        resetTodo();
+        resetAll();
     }
 }
 
 // obtener datos
-function leerDatos() {
-    let datosRegistro = {};
+function readData() {
+    let dataReg = {};
 
-    datosRegistro["nombreAlumno"] = document.getElementById("nombreAlumno").value;
+    dataReg["nombreAlumno"] = document.getElementById("nombreAlumno").value;
 
-    datosRegistro["notaUno"] = document.getElementById("notaUno").value;
+    dataReg["notaUno"] = document.getElementById("notaUno").value;
 
-    datosRegistro["notaDos"] = document.getElementById("notaDos").value;
+    dataReg["notaDos"] = document.getElementById("notaDos").value;
 
-    return datosRegistro;
+    return dataReg;
 
 }
 
 // insertar datos
-function escDatos(datos) {
-    let tablaLista = document.getElementById("lista").getElementsByTagName('tbody')[0];
-    let esclista = tablaLista.insertRow(tablaLista.length);
+function writeData(datos) {
+    let tableList = document.getElementById("lista").getElementsByTagName('tbody')[0];
+    let writeList = tableList.insertRow(tableList.length);
 
-    let regZero = esclista.insertCell(0);
+    let regZero = writeList.insertCell(0);
     regZero.innerHTML = `<i class="fa-solid fa-otter"></i> `;
 
-    let regUno = esclista.insertCell(1);
+    let regUno = writeList.insertCell(1);
     regUno.innerHTML = datos.nombreAlumno;
 
-    let regDos = esclista.insertCell(2);
+    let regDos = writeList.insertCell(2);
     regDos.innerHTML = datos.notaUno;
 
-    let regTres = esclista.insertCell(3);
+    let regTres = writeList.insertCell(3);
     regTres.innerHTML = datos.notaDos;
 
-    let regCuatro = esclista.insertCell(4);
+    let regCuatro = writeList.insertCell(4);
     regCuatro.innerHTML = (parseFloat(datos.notaUno) + parseFloat(datos.notaDos)) / 2;
 
-    let btnEdEl = esclista.insertCell(5);
-    btnEdEl.innerHTML = `<button onClick='editando(this)' class="btn-ed"><i class="fa-solid fa-user-pen"></i></button> <button onClick='borrando(this)' class="btn-del"><i class="fa-solid fa-trash-can"></i></button>`;
+    let btnEdEl = writeList.insertCell(5);
+    btnEdEl.innerHTML = `<button onClick='editData(this)' class="btn-ed"><i class="fa-solid fa-user-pen"></i></button> <button onClick='erase(this)' class="btn-del"><i class="fa-solid fa-trash-can"></i></button>`;
 
 
 }
 
 // editar 
-function editando(edi) {
+function editData(edi) {
     filas = edi.parentElement.parentElement;
 
     document.getElementById("nombreAlumno").value = filas.cells[1].innerHTML;
@@ -98,37 +99,34 @@ function editando(edi) {
 
 }
 
-function actualizando(datosRegistro) {
+function actualize(dataReg) {
     filas.cells[0].innerHTML = `<i class="fa-solid fa-otter"></i> `;
-    filas.cells[1].innerHTML = datosRegistro.nombreAlumno;
-    filas.cells[2].innerHTML = datosRegistro.notaUno;
-    filas.cells[3].innerHTML = datosRegistro.notaDos;
-    filas.cells[4].innerHTML = (parseFloat(datosRegistro.notaUno) + parseFloat(datosRegistro.notaDos)) / 2;
+    filas.cells[1].innerHTML = dataReg.nombreAlumno;
+    filas.cells[2].innerHTML = dataReg.notaUno;
+    filas.cells[3].innerHTML = dataReg.notaDos;
+    filas.cells[4].innerHTML = (parseFloat(dataReg.notaUno) + parseFloat(dataReg.notaDos)) / 2;
 
-    alumnos[filas.rowIndex - 1].nombreAlumno = datosRegistro.nombreAlumno;
-    alumnos[filas.rowIndex - 1].notaUno = datosRegistro.notaUno;
-    alumnos[filas.rowIndex - 1].notaDos = datosRegistro.notaDos;
+    alumnos[filas.rowIndex - 1].nombreAlumno = dataReg.nombreAlumno;
+    alumnos[filas.rowIndex - 1].notaUno = dataReg.notaUno;
+    alumnos[filas.rowIndex - 1].notaDos = dataReg.notaDos;
     localStorage.setItem("alumno", JSON.stringify(alumnos));
     document.getElementById("btnAgregar").style.display = "block";
     document.getElementById("btnEditar").style.display = "none";
 }
 
 // borrar
-function borrando(edi) {
+function erase(edi) {
 
     if (confirm('Confirme que desea eliminar')) {
         borrar = edi.parentElement.parentElement;
 
         alumnos.splice(borrar.rowIndex - 1, 1);
         document.getElementById("lista").deleteRow(borrar.rowIndex);
-        console.log(borrar.rowIndex);
-        console.table(alumnos);
         localStorage.setItem("alumno", JSON.stringify(alumnos));
     }
-    resetTodo();
 }
 
-function resetTodo() {
+function resetAll() {
     document.getElementById("nombreAlumno").value = '';
     document.getElementById("notaUno").value = '';
     document.getElementById("notaDos").value = '';
@@ -142,11 +140,7 @@ function cargarPag() {
         alumnos = c;
     }
 
-    // let nomb = c.sort((f, g) => {
-    //     return f.nombreAlumno > g.nombreAlumno;
-    // })
     cargaDatosLS(c);
-    // console.table(nomb);
     return c;
 }
 
@@ -176,7 +170,7 @@ function cargaDatosLS(c) {
             regCuatroLS.innerHTML = (parseFloat(nota1For) + parseFloat(nota2For)) / 2;
 
             let btnEdElLS = escLS.insertCell(5);
-            btnEdElLS.innerHTML = `<button onClick='editando(this)' class="btn-ed"><i class="fa-solid fa-user-pen"></i></button> <button onClick='borrando(this)' class="btn-del"><i class="fa-solid fa-trash-can"></i></button>`;
+            btnEdElLS.innerHTML = `<button onClick='editData(this)' class="btn-ed"><i class="fa-solid fa-user-pen"></i></button> <button onClick='erase(this)' class="btn-del"><i class="fa-solid fa-trash-can"></i></button>`;
 
            
         }
